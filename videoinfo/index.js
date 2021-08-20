f1e0cc3aad5655d3aeb86bf628dc9dfab693be74
chehin <chehinjuan@gmail.com>
2021-08-20 11:21:37 -0300

    Archivos del curso

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,8 +1,12 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
     const mainWindows = new BrowserWindow({});
     mainWindows.loadURL(`file://${__dirname}/index.html`);
+});
+
+ipcMain.on('video:submit', () => {
+
 });
