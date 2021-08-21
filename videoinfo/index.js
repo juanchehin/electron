5488463d3bc96b7b7a12df0ca932a3390d3c57e5
chehin <chehinjuan@gmail.com>
2021-08-21 10:20:32 -0300

    video example

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,12 +1,20 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
 const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
-    const mainWindows = new BrowserWindow({});
+    const mainWindows = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    });
     mainWindows.loadURL(`file://${__dirname}/index.html`);
 });
 
-ipcMain.on('video:submit', () => {
-
+ipcMain.on('video:submit', (event, path) => {
+    ffmpeg.ffprobe((path, metadata) => {
+        console.log('La duracion es : ', metadata.format.duration);
+    });
 });
