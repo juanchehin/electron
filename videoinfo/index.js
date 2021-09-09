e30c8d5a8f8f2cc8143ea128506bf7dce0530b87
chehin <chehinjuan@gmail.com>
2021-09-09 15:59:28 -0300

    videoinfo funcionando - no olvidar ver google drive

--- videoinfo/index.js
+++ videoinfo/index.js
@@ -1,21 +1,36 @@
 const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg');
+const path = require('path');
+ffmpeg.setFfmpegPath(path.join(__dirname, './ffmpeg-n4.4-150-gb5cdf08cae-win64-gpl-4.4/bin/ffprobe'));
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg');
+// const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegPath);
 
 const { app, BrowserWindow, ipcMain } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
-    mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    });
     console.log('pasa app.on');
 
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
 ipcMain.on('video:submit', (event, path) => {
-    console.log('pasa ipcMain.on');
+    console.log('pasa ipcMain.on y path es : ', path);
 
-    ffmpeg.ffprobe((path, metadata) => {
-        mainWindow.webContents.send('video:metadata', metadata.format.duration);
+    // ffmpeg.ffprobe((path, metadata) => {
+    //     mainWindow.webContents.send('video:metadata', metadata.format.duration);
+    //     console.log('pasa');
+    // });
+    ffmpeg.ffprobe(path, function(err, metadata) {
+        console.log(err);
+        mainWindow.webContents.send('video:metadata', metadata);
     });
 });
