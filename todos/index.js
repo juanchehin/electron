fb7365af55ffddd276fbe9b795c2566228c5d143
chehin <chehinjuan@gmail.com>
2021-09-23 12:00:33 -0300

    Nueva funcion en menu + ventana de add-todo

--- todos/index.js
+++ todos/index.js
@@ -3,8 +3,10 @@ const electron = require('electron');
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', () => {
+    console.log("pasa");
     mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/main.html`);
 
@@ -12,6 +14,33 @@ app.on('ready', () => {
     Menu.setApplicationMenu(mainMenu);
 });
 
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add New todo'
+    });
+    addWindow.loadURL(`file://${__dirname}/add.html`);
+}
+
+console.log("pasa 1");
 const menuTemplate = [{
-    label: 'File'
+    label: 'File',
+    submenu: [{
+            label: 'New todo',
+            click() { createAddWindow(); }
+        },
+        {
+            label: 'Quit',
+            accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+            click() {
+                app.quit();
+            }
+        }
+    ]
 }];
+
+if (process.platform === 'darwin') {
+    console.log("pasa 2");
+    menuTemplate.unshift({})
+}
