5a063ca6bd3454d29d8cb58b916fe634ba5eb5f8
chehin <chehinjuan@gmail.com>
2021-09-24 21:03:09 -0300

    Se agrego funcion para crear un item - Comunicacion entre ventana principal y secundaria

--- todos/index.js
+++ todos/index.js
@@ -1,14 +1,20 @@
 const electron = require('electron');
 
-const { app, BrowserWindow, Menu } = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
 let addWindow;
 
 app.on('ready', () => {
-    console.log("pasa");
-    mainWindow = new BrowserWindow({});
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        },
+    });
     mainWindow.loadURL(`file://${__dirname}/main.html`);
+    mainWindow.on('closed', () => app.quit());
 
     const mainMenu = Menu.buildFromTemplate(menuTemplate);
     Menu.setApplicationMenu(mainMenu);
@@ -18,12 +24,22 @@ function createAddWindow() {
     addWindow = new BrowserWindow({
         width: 300,
         height: 200,
-        title: 'Add New todo'
+        title: 'Add New todo',
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        }
     });
     addWindow.loadURL(`file://${__dirname}/add.html`);
+    addWindow.on('closed', () => addWindow = null);
 }
 
-console.log("pasa 1");
+ipcMain.on('todo:add', (event, todo) => {
+    mainWindow.webContents.send('todo:add', todo);
+    addWindow.close();
+})
+
 const menuTemplate = [{
     label: 'File',
     submenu: [{
@@ -41,6 +57,21 @@ const menuTemplate = [{
 }];
 
 if (process.platform === 'darwin') {
-    console.log("pasa 2");
     menuTemplate.unshift({})
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    menuTemplate.push({
+        label: 'View',
+        submenu: [
+            { role: 'reload' },
+            {
+                label: 'Toogle Developers Tools',
+                accelerator: process.platform === 'darwin' ? 'Command+Alt+I' : 'Ctrl+Shift+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            }
+        ]
+    })
 }
