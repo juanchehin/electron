b4943fc5b1dbca16baa5626660b0b3730c285114
chehin <chehinjuan@gmail.com>
2021-09-26 12:08:02 -0300

    Ajustes de posicionamiento de la ventana

--- course/boilerplates/tasky/index.js
+++ course/boilerplates/tasky/index.js
@@ -4,17 +4,40 @@ const electron = require('electron');
 const { app, BrowserWindow, Tray } = electron;
 
 let mainWindow;
+let tray;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({
         height: 500,
         width: 300,
         frame: false,
-        resizable: false
+        resizable: false,
+        show: false
     });
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
 
     const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
     const iconPath = path.join(__dirname, `/src/assets/${iconName}`);
-    new Tray(iconPath);
+    tray = new Tray(iconPath);
+
+    tray.on('click', (event, bounds) => {
+        // Limites de evento clic
+        const { x, y } = bounds;
+
+        // Alto y ancho de la ventana
+        const { height, width } = mainWindow.getBounds();
+
+        if (mainWindow.isVisible()) {
+            mainWindow.hide();
+        } else {
+            const yPosition = process.platform === 'darwin' ? y : y - height;
+            mainWindow.setBounds({
+                x: x - width / 2,
+                y: yPosition,
+                width,
+                height
+            });
+            mainWindow.show();
+        }
+    });
 });
