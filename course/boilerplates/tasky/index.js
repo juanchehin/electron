294d20b969b7f7316c7b19e3ec6dab1b9dedd43b
chehin <chehinjuan@gmail.com>
2021-09-27 09:15:05 -0300

    Ocultar icono en barra de aplicaciones

--- course/boilerplates/tasky/index.js
+++ course/boilerplates/tasky/index.js
@@ -7,12 +7,14 @@ const { app, BrowserWindow, Tray } = electron;
 let mainWindow;
 
 app.on('ready', () => {
+    // app.dock.hide(); // Para MAC OS
     mainWindow = new BrowserWindow({
         height: 500,
         width: 300,
         frame: false,
         resizable: false,
-        show: false
+        show: false,
+        skipTaskbar: true
     });
     mainWindow.loadURL(`file://${__dirname}/src/index.html`);
     mainWindow.on('blur', () => {
